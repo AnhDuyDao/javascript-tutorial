c69389f5d560a5d8ee9c2e06d42964bbc8965b91
Duy Anh <duyanhdao297@gmail.com>
2025-06-05 22:05:42 +0700

    Fundamentals 1 Operator precedence

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,69 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+// Variable name conventions
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof true);
+// console.log(typeof "Jonas");
+
+javaScriptIsFun = "YES!";
+console.log(javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1999;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+// Using let to declare variables and reassign values
+let age = 30;
+age = 31;
+
+// Using const to declare variables that should not be reassigned
+const birthYear = 1991;
+//birthYear = 1990;
+//const job;
+
+var job = "programmer";
+job = "teacher";
+*/
+
+const now = 2025;
+const ageAnh = now - 1999;
+const ageSarah = now - 2009;
+console.log(ageAnh, ageSarah);
+
+console.log(ageAnh * 2, ageAnh / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2 = 8
+const firstName = "Anh";
+const lastName = "Duy"
+console.log(firstName + " " + lastName); // String concatenation
+console.log(firstName, lastName); // Anh Duy still have a space in between because this is not a string concatenation
+
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+x--; // x = x - 1 = 99
+console.log(x);
+
+// Comparison operators
+console.log(ageAnh > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1999 > now - 2009); // true
