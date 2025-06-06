d15eb078556263865bb76cb6ee8b2f28c8705bd4
Duy Anh <duyanhdao297@gmail.com>
2025-06-07 01:30:42 +0700

    Fundamentals 2 Strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,16 @@
+// Strict mode, always at the top of the file
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+    hasDriversLicense = true;
+}
+
+if (hasDriversLicense) {
+    console.log('I can drive :D');
+}
+
+//const interface = 'Audio'; // 'interface' is a reserved keyword in JavaScript, so it should not be used as a variable name
+//const private = 534; // 'private' is also a reserved keyword, so it should not be used as a variable name
