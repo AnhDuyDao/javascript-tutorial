aa3eec60affefa6c2d9c404496b7ea887362bf85
Duy Anh <duyanhdao297@gmail.com>
2025-06-07 02:00:58 +0700

    Fundamentals 2 Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,6 @@
 // Strict mode, always at the top of the file
 'use strict';
-
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -14,3 +14,23 @@ if (hasDriversLicense) {
 
 //const interface = 'Audio'; // 'interface' is a reserved keyword in JavaScript, so it should not be used as a variable name
 //const private = 534; // 'private' is also a reserved keyword, so it should not be used as a variable name
+*/
+
+function logger() {
+    console.log('My name is Duy Anh');
+}
+// Calling / running / invoking function
+logger();
+console.log(typeof (logger())); // return undefined value (one of five falsy value)
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
