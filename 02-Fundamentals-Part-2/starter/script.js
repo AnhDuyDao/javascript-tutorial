1da726d276ccbe2a1c9afa02141cdb0c053f879f
Duy Anh <duyanhdao297@gmail.com>
2025-06-09 15:45:56 +0700

    Fundamentals 2 Arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -75,7 +75,7 @@ function fruitProcessor(apples, oranges) {
     return juice;
 }
 console.log(fruitProcessor(2, 3));
-*/
+
 const calcAge = function (birthYear) {
     return 2037 - birthYear;
 }
@@ -97,3 +97,34 @@ const yearUntilRetirement = function (birthYear, firstName) {
 }
 console.log(yearUntilRetirement(1991, 'Duy Anh'));
 console.log(yearUntilRetirement(1970, 'Mike'));
+*/
+const friends = ['Micheal', 'Steven', 'Peter'];
+console.log(friends);
+//const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+// friends = ['Bob', 'Alice']; // Error: Cannot assign to 'friends' because it is a constant
+
+const firstName = 'Anh';
+const anh = [firstName, 'Duy', 2037 - 1991, 'student', friends];
+
+console.log(anh);
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
