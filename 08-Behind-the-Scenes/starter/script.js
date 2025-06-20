4281a5e6b1d0d108ec73d11eb626f521336207b4
Duy Anh <duyanhdao297@gmail.com>
2025-06-20 14:02:45 +0700

    feat: hoist and tdz

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function calcAge(birthYear) {
    const age = 2037 - birthYear;
    function printAge() {
@@ -24,7 +24,7 @@ function calcAge(birthYear) {
          }
       }
       //console.log(str);
-      console.log(millennial);
+      console.log(millennial); // print true because 'var' is function-scoped, so it can be accessed here
 
       // Always use 'use strict' mode
       //console.log(add(2, 3)); // If you dont use 'use strict', this will work
@@ -38,3 +38,39 @@ const firstName = 'Anh';
 calcAge(1991);
 // console.log(age); // This will throw an error because age is not defined in this scope
 // printAge(); // This will throw an error because printAge is not defined in this scope
+*/
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Anh';
+let job = 'Software Engineer';
+const year = 1991;
+
+console.log(addDecl(2, 3)); // This will work because function declarations are hoisted
+// console.log(addExpr(2, 3)); // This will throw an error because function expressions are not hoisted
+console.log(addArrow); // Return undefined
+// console.log(addArrow(2, 3)); // This will throw an error because arrow functions are not hoisted
+
+function addDecl(a, b) {
+   return a + b;
+}
+
+// With var, function expressions are hoisted but not initialized, it becomes undefined
+// So calling undefined(2,3) will throw an error
+// TypeError: undefined is not a function
+const addExpr = function (a, b) {
+   return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+
+if (!numProducts) deleteShoppingCart();
+var numProducts = 10;
+
+function deleteShoppingCart() {
+   console.log('All products deleted!');
+}
